e5a22d9a4e98164147e2196f6c43cad3f8116f60
Турдишаев Закиржан <turdisaevzakirzan@MacBook-Air-Turdisaev.local>
2026-02-09 09:44:16 +0500

    Added lam

--- Lambda/lambda.py
+++ Lambda/lambda.py
@@ -17,3 +17,15 @@ mydoubler = myfunc(2)
 
 print(mydoubler(11))
 
+students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
+sorted_students = sorted(students, key=lambda x: x[1])
+print(sorted_students)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
+
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
+
