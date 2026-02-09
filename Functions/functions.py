35f3c289f78327f360e9c45120ce92227aafa677
Турдишаев Закиржан <turdisaevzakirzan@MacBook-Air-Turdisaev.local>
2026-02-09 09:29:30 +0500

    Added functions

---
+++ Functions/functions.py
@@ -0,0 +1,34 @@
+def my_function():
+  print("Hello from a function")
+
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
+
+temp2 = 95
+celsius2 = (temp2 - 32) * 5 / 9
+print(celsius2)
+
+temp3 = 50
+celsius3 = (temp3 - 32) * 5 / 9
+print(celsius3)
+
+def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message)
